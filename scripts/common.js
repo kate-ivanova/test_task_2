a8e31fd2ea00275d1b7e7e2373912f36b2363866
Ekaterina Ivanova <ekaterina.ivanova@saltpepper.ru>
2015-09-30 10:41:38 +0300

    Distribution Commit

---
+++ scripts/common.js
@@ -0,0 +1,10 @@
+(function() {
+  define('common.js',['require','exports','module'],function(require, exports, module) {
+    return window.common = {
+      app: null,
+      router: null
+    };
+  });
+
+}).call(this);
+
